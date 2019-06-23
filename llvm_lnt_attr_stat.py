3a14c826971c69b9b634f126c1181224492ef2f3
sstefan1 <sstipanovic@s-energize.com>
2019-06-23 11:51:28 +0200

    cleanup

--- llvm_lnt_attr_stat.py
+++ llvm_lnt_attr_stat.py
@@ -1,5 +1,4 @@
 import json, sys
-from pprint import pprint
 
 nounwind_attributor = 0
 nounwind_functionattrs = 0
@@ -8,19 +7,19 @@ with open(sys.argv[1]) as f:
     data = json.load(f)
 
     for key1, value1 in data.items():
-        print key1
-        if key1 == 'tests':
-            for test in value1:
-                # pprint(test)
-                for key, value in test.items():
-                    if key == 'metrics':
-                        for attr, strnum in value.items():
-                            if attr.encode("ascii") == "attributor.NumFnNoUnwind":
-                                num = float(strnum)
-                                nounwind_attributor += num
-                            elif(attr.encode("ascii") == "functionattrs.NumNoUnwind"):
-                                num = float(strnum)
-                                nounwind_functionattrs += num
+        if key1 != 'tests':
+            continue
+        for test in value1:
+            for key, value in test.items():
+                if key != 'metrics':
+                    continue
+                for attr, strnum in value.items():
+                    if attr.encode("ascii") == "attributor.NumFnNoUnwind":
+                        num = float(strnum)
+                        nounwind_attributor += num
+                    elif(attr.encode("ascii") == "functionattrs.NumNoUnwind"):
+                        num = float(strnum)
+                        nounwind_functionattrs += num
 
 print("attributor.NumFnNoUnwind: " + str(nounwind_attributor))
 print("functionattrs.NumNoUnwind: " + str(nounwind_functionattrs))
